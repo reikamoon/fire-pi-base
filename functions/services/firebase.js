01fff8a3ee0097260cd3c0c3ad91264442369a52
Christopher Barnes <chris.barnes.2000@me.com>
2021-07-22 16:42:47 -0700

    [Update]: Firbase Functions & Rules

---
+++ functions/services/firebase.js
@@ -0,0 +1,17 @@
+const admin = require("firebase-admin")
+const serviceAccount = require("./serviceKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+})
+
+exports.db = admin.database();
+exports.firebase = admin
