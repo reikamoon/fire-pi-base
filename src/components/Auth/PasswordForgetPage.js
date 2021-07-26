1c29cd9d56bfbac5f2c04d7e79ca42b2d8f23f0c
Anjelica <ambrosio.anjelica@gmail.com>
2021-07-26 16:23:41 -0700

    added .env

--- src/components/Auth/PasswordForgetPage.js
+++ src/components/Auth/PasswordForgetPage.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom'
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';  
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../routes';
 
